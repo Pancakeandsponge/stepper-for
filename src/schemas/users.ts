c3685d1e08a39b9e54414238c0048d40d360d296
Fatima Asadie <adtfatyma3@gmail.com>
2025-11-28 23:38:48 +0330

    feat: add a schema for each step

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -5,5 +5,17 @@ export const userSchema = z.object({
     email :z.email({error:'please enter your email'}),
     address:z.string({error:'please enter your address'})
     });
+
+
+export const firstStepSchema = z.object({
+    name:z.string().min(3,{error: 'please enter your name'}),
+    email :z.email({error:'please enter your email'}),
+})
+export const secondStepSchema = z.object({
+     address:z.string({error:'please enter your address'})
+})
     
 export type TuserSchema = z.infer<typeof userSchema>
+export type TfirstStepSchema = z.infer<typeof firstStepSchema>
+export type TsecondStepSchema = z.infer<typeof secondStepSchema>
+
